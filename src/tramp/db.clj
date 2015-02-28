57fa19210a21afb66043b5b0394701b7977716b4
Luke Snape <lukesnape@Lukes-MacBook-Pro.local>
2015-02-28 13:37:38 +0000

    Nil checking before calling monger/batch-insert!

--- src/tramp/db.clj
+++ src/tramp/db.clj
@@ -8,12 +8,13 @@
   (monger/connect (:mongo-db (config))))
 
 (defn insert-listings! [listings]
-  (let [conn (mongo-conn)
-        {:keys [host port db-name coll]} (:mongo-db (config))
-        db (monger/get-db (mongo-conn) db-name)]
+  (when (seq listings)
+    (let [conn (mongo-conn)
+          {:keys [host port db-name coll]} (:mongo-db (config))
+          db (monger/get-db (mongo-conn) db-name)]
 
-    (map #(assoc % :_id (ObjectId.)) listings)
-    (coll/insert-batch db coll listings)))
+      (map #(assoc % :_id (ObjectId.)) listings)
+      (coll/insert-batch db coll listings))))
 
 (defn seen-listing? [{:keys [listing-id] :as listing}]
   (let [{:keys [host port db-name coll]} (:mongo-db (config))
